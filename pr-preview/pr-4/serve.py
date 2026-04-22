bf69dac5b88b2acdc24f252939bdde4e1e7c55a8
Lucopio <Lucopio@users.noreply.github.com>
2026-04-22 00:47:56 +0000

    Remove preview for PR 4 🛬

--- pr-preview/pr-4/serve.py
+++
@@ -1,3 +0,0 @@
-import http.server, os
-os.chdir(r"G:\Mi unidad\Otros proyectos\AoraLive\aora-live\aora-live")
-http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, port=3000, bind='')
